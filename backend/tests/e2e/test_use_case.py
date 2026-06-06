0650fa8d04c2c3ad4205ce0f933f4f1853a54f2e
PolkovnikLetov <56263477+PolkovnikLetov@users.noreply.github.com>
2026-06-06 19:50:05 +0500

    Backend/tests (#4)
    * tests
    
    * req
    
    ---------
    
    Co-authored-by: user 100 <user100@or-c4.perm.21-school.ru>

---
+++ backend/tests/e2e/test_use_case.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.mark.e2e
+@pytest.mark.asyncio
+def full_user_registration_test():
+    pass
---
+++ backend/tests/integration/test_api_end_points.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.mark.integration
+@pytest.mark.asyncio
+def db_session_test()
+    pass
